5a2253a963b1cd110179ab571e4ade8b079b4630
Olajiive <muizolatunji29@gmail.com>
2023-03-19 19:57:09 -0700

    added jwt loader

--- Api/__init__.py
+++ Api/__init__.py
@@ -45,11 +45,13 @@ def create_app(config=config_dict["devconfig"]):
             ),
             401,
         )
+    
     @jwt.additional_claims_loader
     def add_additional_claims(identity):
         if identity == 1:
             return {"is_admin": True}
         return {"is_admin": False}
+    
     @jwt.needs_fresh_token_loader
     def token_not_fresh_callback(jwt_header, jwt_payload):
         return (
@@ -61,17 +63,18 @@ def create_app(config=config_dict["devconfig"]):
             ),
             401,
         )
-    # this is going to check the BLOCKLIST set if the token you're trying to use exist in the set
-    # if the token is present in the set, it will return a 'token revoked' message
+
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
         return jwt_payload["jti"] in BLOCKLIST
+    
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
             jsonify({"message": "The token has expired.", "error": "token_expired"}),
             401,
         )
+    
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return (
@@ -80,6 +83,7 @@ def create_app(config=config_dict["devconfig"]):
             ),
             401,
         )
+    
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return (
